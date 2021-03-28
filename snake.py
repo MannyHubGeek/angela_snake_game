d3b65935e25d0f59463635cc8aadd71fc6cbb607
emmanuelohue <ohueus@yahoo.com>
2021-03-28 22:25:01 +0100

    Game is now complete

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
@@ -6,20 +7,32 @@ DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+
 class Snake:
-    #The code here should determine what should happen when a new object of snake is created
+    # The code here should determine what should happen when a new object of snake is created
     def __init__(self):
-        self.segments =[]
+        self.segments = []
         self.create_snake()
         self.head = self.segments[0]
 
     def create_snake(self):
-            for position in STARTING_POSITIONS:
-                new_segment = Turtle("square")
-                new_segment.color("white")
-                new_segment.penup()
-                new_segment.goto(position)
-                self.segments.append(new_segment)
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+        self.segments.append(new_segment)
+
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -27,7 +40,7 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_X, new_y)
         self.head.forward(MOVE_DISTANCE)
-        
+
     def up(self):
         self.head.setheading(UP)
 
@@ -39,10 +52,3 @@ class Snake:
 
     def right(self):
         self.head.setheading(RIGHT)
-
-
-
-
-
-
-
